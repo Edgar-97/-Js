31f6610c04618deb3adf45e87e63e417548f2786
Edgar <edgarbabayan75@mail.ru>
2021-05-09 19:52:01 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + + "4");
+
+let incr = 10,
+    decr = 10;
+
+    // ++incr;
+    // --decr;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    const isChecked = false,
+            isClose = false;
+
+    console.log(isChecked || !isClose);
+
+
+
+
